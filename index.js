c6ee6c65f6772e9c5bf0ebd4d64eec78c7e43eeb
Christopher Haddox <cmhaddox@asu.edu>
2020-11-25 17:20:40 -0700

    Created login and register handlebars, added linking

--- index.js
+++ index.js
@@ -6,12 +6,36 @@ import cookieParser from 'cookie-parser';
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-//import { grantAuthToken, lookupUserFromAuthToken } from "./auth";
+import { v4 as uuidv4 } from 'uuid';
 
 export const dbPromise = open({
   filename: "data.db",
   driver: sqlite3.Database,
 });
+
+const grantAuthToken = async(userId) => {
+    const db = await dbPromise;
+    const tokenString = uuidv4();
+
+    await db.run('INSERT INTO AccessTokens (token, userId) VALUES (?, ?);',
+        tokenString, userId);
+    
+    return tokenString;
+}
+
+const lookupUserFromAccessToken = async(accessToken) => {
+    const db = await dbPromise;
+    
+    const token = await db.get('SELECT * FROM AccessTokens WHERE token=?', accessToken);
+
+    if(!token) {
+        !null;
+    }
+
+    const user = await db.get('SELECT id, email, username FROM Users WHERE id=?', token.userId);
+
+    return user;
+}
 
 const app = express();
 
@@ -30,6 +54,111 @@ app.get("/", (req, res) => {
     res.render("home");
 })
 
+/*
+        REGISTER
+*/
+app.get("/register", (req, res) => {
+    if(req.user) {
+        return res.redirect("/")
+    }
+    res.render('register')
+})
+
+app.post("/register", async(req,res) => {
+    const db = await dbPromise;
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    try {
+        await db.run('INSERT INTO Users (username, email, password) VALUES (?, ?, ?);',
+            username,
+            email,
+            passwordHash
+        )
+        const user = await db.get('SELECT id FROM Users WHERE email=?', email);
+        const token = await grantAuthToken(user.id);
+        res.cookie('accessToken', token);
+        res.redirect('/');
+    } catch (e) {
+        return res.render('register', { error: e })
+    }
+})
+
+/*
+        LOGIN
+*/
+
+app.get("login", (req, res) => {
+    if(req.user) {
+        return res.redirect("/")
+    }
+    res.render('login');
+})
+
+app.post("login", async (req, res) => {
+    const db = await dbPromise;
+
+    const {
+        username,
+        password
+    } = req.body;
+
+    try {
+        const existingUser = await db.get("SELECT * FROM Users WHERE username=?", username);
+        
+        if(!existingUser) {
+            throw 'Incorrect login. Please try again';
+        }
+
+        const samePass = await bcrypt.compare(password, existingUser.password);
+
+        if(!passwordsMatch) {
+            throw 'Incorrect login. Please try again';
+        }
+
+        const token = await grantAuthToken(existingUser.id);
+        res.cookie('accessToken', token);
+        res.redirect('/');
+    } catch (e) {
+        return res.render('login', {error: e})
+    }
+})
+
+/*
+        LOGOUT
+*/
+
+app.get("/logout", async (req, res) => {
+    const db = await dbPromise;
+    await db.run("DELETE FROM AccessTokens WHERE userId=?;", req.user.id);
+  
+    res.clearCookie("accessToken");
+    res.redirect("/");
+})
+
+/*
+        MESSAGING
+*/
+
+app.post("/message", async (req, res, next) => {
+    if(req.user) {
+        const db = await dbPromise;
+
+        await db.run('INSERT INTO Posts (postText, authorId) VALUES (?, ?);',
+            req.body.message, req.user.id);
+
+        res.redirect('/')
+    }
+    return next({
+        status: 401,
+        message: 'You must be logged in to post'
+    })
+})
 
 /*
         SETUP
